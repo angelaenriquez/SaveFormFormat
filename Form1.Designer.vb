f8b1ac598e1f63b01adbcd81e5da4dc0f101dd25
angelaenriquez <angelaenriquez2699@gmail.com>
2020-02-15 14:53:15 +0800

    Functionality added

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -22,29 +22,30 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.NameInput = New System.Windows.Forms.TextBox()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
-        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.AgeInput = New System.Windows.Forms.TextBox()
         Me.Label3 = New System.Windows.Forms.Label()
-        Me.TextBox3 = New System.Windows.Forms.TextBox()
-        Me.Button1 = New System.Windows.Forms.Button()
-        Me.Button2 = New System.Windows.Forms.Button()
-        Me.Button3 = New System.Windows.Forms.Button()
+        Me.AddressInput = New System.Windows.Forms.TextBox()
+        Me.SaveTxt = New System.Windows.Forms.Button()
+        Me.SaveXml = New System.Windows.Forms.Button()
+        Me.SaveJson = New System.Windows.Forms.Button()
+        Me.Status = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
-        'TextBox1
+        'NameInput
         '
-        Me.TextBox1.Location = New System.Drawing.Point(107, 28)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(203, 20)
-        Me.TextBox1.TabIndex = 0
+        Me.NameInput.Location = New System.Drawing.Point(107, 65)
+        Me.NameInput.Name = "NameInput"
+        Me.NameInput.Size = New System.Drawing.Size(203, 20)
+        Me.NameInput.TabIndex = 0
         '
         'Label1
         '
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.0!)
-        Me.Label1.Location = New System.Drawing.Point(35, 27)
+        Me.Label1.Location = New System.Drawing.Point(35, 64)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(52, 18)
         Me.Label1.TabIndex = 1
@@ -54,83 +55,92 @@ Partial Class Form1
         '
         Me.Label2.AutoSize = True
         Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.0!)
-        Me.Label2.Location = New System.Drawing.Point(35, 65)
+        Me.Label2.Location = New System.Drawing.Point(35, 102)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(37, 18)
         Me.Label2.TabIndex = 3
         Me.Label2.Text = "Age:"
         '
-        'TextBox2
+        'AgeInput
         '
-        Me.TextBox2.Location = New System.Drawing.Point(107, 65)
-        Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(58, 20)
-        Me.TextBox2.TabIndex = 2
+        Me.AgeInput.Location = New System.Drawing.Point(107, 102)
+        Me.AgeInput.Name = "AgeInput"
+        Me.AgeInput.Size = New System.Drawing.Size(58, 20)
+        Me.AgeInput.TabIndex = 2
         '
         'Label3
         '
         Me.Label3.AutoSize = True
         Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.0!)
-        Me.Label3.Location = New System.Drawing.Point(35, 105)
+        Me.Label3.Location = New System.Drawing.Point(35, 142)
         Me.Label3.Name = "Label3"
         Me.Label3.Size = New System.Drawing.Size(66, 18)
         Me.Label3.TabIndex = 5
         Me.Label3.Text = "Address:"
         '
-        'TextBox3
+        'AddressInput
         '
-        Me.TextBox3.Location = New System.Drawing.Point(107, 106)
-        Me.TextBox3.Name = "TextBox3"
-        Me.TextBox3.Size = New System.Drawing.Size(230, 20)
-        Me.TextBox3.TabIndex = 4
+        Me.AddressInput.Location = New System.Drawing.Point(107, 143)
+        Me.AddressInput.Name = "AddressInput"
+        Me.AddressInput.Size = New System.Drawing.Size(230, 20)
+        Me.AddressInput.TabIndex = 4
         '
-        'Button1
+        'SaveTxt
         '
-        Me.Button1.BackColor = System.Drawing.SystemColors.ActiveBorder
-        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.0!)
-        Me.Button1.Location = New System.Drawing.Point(38, 155)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 32)
-        Me.Button1.TabIndex = 6
-        Me.Button1.Text = ".txt"
-        Me.Button1.UseVisualStyleBackColor = False
+        Me.SaveTxt.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.SaveTxt.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.0!)
+        Me.SaveTxt.Location = New System.Drawing.Point(38, 192)
+        Me.SaveTxt.Name = "SaveTxt"
+        Me.SaveTxt.Size = New System.Drawing.Size(75, 32)
+        Me.SaveTxt.TabIndex = 6
+        Me.SaveTxt.Text = ".txt"
+        Me.SaveTxt.UseVisualStyleBackColor = False
         '
-        'Button2
+        'SaveXml
         '
-        Me.Button2.BackColor = System.Drawing.SystemColors.ActiveBorder
-        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.0!)
-        Me.Button2.Location = New System.Drawing.Point(151, 155)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(75, 32)
-        Me.Button2.TabIndex = 7
-        Me.Button2.Text = ".xml"
-        Me.Button2.UseVisualStyleBackColor = False
+        Me.SaveXml.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.SaveXml.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.0!)
+        Me.SaveXml.Location = New System.Drawing.Point(151, 192)
+        Me.SaveXml.Name = "SaveXml"
+        Me.SaveXml.Size = New System.Drawing.Size(75, 32)
+        Me.SaveXml.TabIndex = 7
+        Me.SaveXml.Text = ".xml"
+        Me.SaveXml.UseVisualStyleBackColor = False
         '
-        'Button3
+        'SaveJson
         '
-        Me.Button3.BackColor = System.Drawing.SystemColors.ActiveBorder
-        Me.Button3.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.0!)
-        Me.Button3.Location = New System.Drawing.Point(262, 155)
-        Me.Button3.Name = "Button3"
-        Me.Button3.Size = New System.Drawing.Size(75, 32)
-        Me.Button3.TabIndex = 8
-        Me.Button3.Text = ".json"
-        Me.Button3.UseVisualStyleBackColor = False
+        Me.SaveJson.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.SaveJson.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.0!)
+        Me.SaveJson.Location = New System.Drawing.Point(262, 192)
+        Me.SaveJson.Name = "SaveJson"
+        Me.SaveJson.Size = New System.Drawing.Size(75, 32)
+        Me.SaveJson.TabIndex = 8
+        Me.SaveJson.Text = ".json"
+        Me.SaveJson.UseVisualStyleBackColor = False
+        '
+        'Status
+        '
+        Me.Status.AutoSize = True
+        Me.Status.Location = New System.Drawing.Point(91, 28)
+        Me.Status.Name = "Status"
+        Me.Status.Size = New System.Drawing.Size(0, 13)
+        Me.Status.TabIndex = 9
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(377, 199)
-        Me.Controls.Add(Me.Button3)
-        Me.Controls.Add(Me.Button2)
-        Me.Controls.Add(Me.Button1)
+        Me.ClientSize = New System.Drawing.Size(377, 243)
+        Me.Controls.Add(Me.Status)
+        Me.Controls.Add(Me.SaveJson)
+        Me.Controls.Add(Me.SaveXml)
+        Me.Controls.Add(Me.SaveTxt)
         Me.Controls.Add(Me.Label3)
-        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.AddressInput)
         Me.Controls.Add(Me.Label2)
-        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.AgeInput)
         Me.Controls.Add(Me.Label1)
-        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.NameInput)
         Me.Name = "Form1"
         Me.Text = "Save Form Format"
         Me.ResumeLayout(False)
@@ -138,13 +148,14 @@ Partial Class Form1
 
     End Sub
 
-    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents NameInput As TextBox
     Friend WithEvents Label1 As Label
     Friend WithEvents Label2 As Label
-    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents AgeInput As TextBox
     Friend WithEvents Label3 As Label
-    Friend WithEvents TextBox3 As TextBox
-    Friend WithEvents Button1 As Button
-    Friend WithEvents Button2 As Button
-    Friend WithEvents Button3 As Button
+    Friend WithEvents AddressInput As TextBox
+    Friend WithEvents SaveTxt As Button
+    Friend WithEvents SaveXml As Button
+    Friend WithEvents SaveJson As Button
+    Friend WithEvents Status As Label
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,9 +1,94 @@
-﻿Public Class Form1
-    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+﻿Imports System.Text.RegularExpressions
+Imports Newtonsoft.Json
 
+Public Class Form1
+    Dim PATH As String = "C:/Users/ENRIQUEZ/Desktop/Samples"
+
+    Class formInput
+        Public name As String
+        Public age As Int32
+        Public address As String
+    End Class
+
+    Private Sub TextAge_KeyPress(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles AgeInput.KeyPress
+        If Not Char.IsNumber(e.KeyChar) AndAlso Not Char.IsControl(e.KeyChar) Then
+            e.Handled = True
+        End If
     End Sub
 
-    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
-
+    Private Sub TextAge_TextChanged(ByVal sender As Object, ByVal e As System.EventArgs) Handles AgeInput.TextChanged
+        Dim digitsOnly As Regex = New Regex("[^\d]")
+        AgeInput.Text = digitsOnly.Replace(AgeInput.Text, "")
     End Sub
+
+    Private Sub SaveTxt_Click(sender As Object, e As EventArgs) Handles SaveTxt.Click
+        If isBlank() Then
+            My.Computer.FileSystem.WriteAllText(PATH & ".txt", "", True)
+            Dim objWriter As New System.IO.StreamWriter(PATH & ".txt")
+
+            objWriter.WriteLine("Name:" & ControlChars.Tab & ControlChars.Tab & NameInput.Text)
+            objWriter.WriteLine("Age:" & ControlChars.Tab & ControlChars.Tab & AgeInput.Text)
+            objWriter.WriteLine("Address:" & ControlChars.Tab & AddressInput.Text)
+
+            objWriter.Close()
+
+            Status.ForeColor = Color.Green
+            Status.Text = "Successfully saved as TXT format."
+        Else
+            Status.ForeColor = Color.Red
+            Status.Text = "Please fill up all inputs."
+        End If
+    End Sub
+
+    Private Sub SaveXml_Click(sender As Object, e As EventArgs) Handles SaveXml.Click
+        If isBlank() Then
+            Dim xmlDeclaration As New XDeclaration("1.0", "UTF-8", "yes")
+            Dim doc As XDocument =
+                New XDocument(xmlDeclaration,
+                              New XElement("Form",
+                                           New XElement("name", "" & NameInput.Text),
+                                           New XElement("age", "" & AgeInput.Text),
+                                           New XElement("address", "" & AddressInput.Text)
+                                          )
+                              )
+
+            doc.Save(PATH & ".xml")
+
+            Status.ForeColor = Color.Green
+            Status.Text = "Successfully saved as XML format."
+        Else
+            Status.ForeColor = Color.Red
+            Status.Text = "Please fill up all inputs."
+        End If
+    End Sub
+
+    Private Sub SaveJson_Click(sender As Object, e As EventArgs) Handles SaveJson.Click
+        If isBlank() Then
+            Dim inp As formInput = New formInput()
+            inp.name = NameInput.Text
+            inp.age = Convert.ToInt32(AgeInput.Text)
+            inp.address = AddressInput.Text
+
+            My.Computer.FileSystem.WriteAllText(PATH & "JSON.txt", JsonConvert.SerializeObject(inp, Newtonsoft.Json.Formatting.Indented), False)
+
+            Status.ForeColor = Color.Green
+            Status.Text = "Successfully saved as JSON format."
+        Else
+            Status.ForeColor = Color.Red
+            Status.Text = "Please fill up all inputs."
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Status.ForeColor = Color.DarkOrange
+        Status.Text = "Please Fill up the Form"
+    End Sub
+
+    Function isBlank() As Boolean
+        If NameInput.Text = "" Or AgeInput.Text = "" Or AddressInput.Text = "" Then
+            isBlank = False
+        Else
+            isBlank = True
+        End If
+    End Function
 End Class
